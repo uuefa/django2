d41e6a30493cc2dfa82f1d6006afd851e85408a3
uuefa <phuri.kehanadee.com>
2024-09-18 16:03:58 +0700

    update

--- myweb/blogs/views.py
+++ myweb/blogs/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Blog
+
 
 def blogs(request):
+  blogs= Blog.objects.all().values()
   template = loader.get_template('blogs.html')
-  return HttpResponse(template.render())
+  context = {
+  'blogs': blogs,
+  }
+  return HttpResponse(template.render(context,request))
--- myweb/members/views.py
+++ myweb/members/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
+  mymembers = Member.objects.all().values()
   template = loader.get_template('member.html')
-  return HttpResponse(template.render())
+  context = {
+  'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
